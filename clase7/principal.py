580e8d284fa8a4d70b2a264762c91bd64c89ab80
Roberto Cruz Lozano <rcruz15@alumnos.uaq.mx>
2021-06-20 12:50:01 -0500

    Update principal.py

--- clase7/principal.py
+++ clase7/principal.py
@@ -1,7 +1,6 @@
 import calculadora
 #from calculadora import suma as nueva_suma
 #from calculadora import resta,multiplicacion,division
-"""
 #resultado = calculadora.suma(10,20)
 resultado = nueva_suma(10,20)
 print(resultado)
@@ -14,6 +13,5 @@ print(resultado)
 #resultado = calculadora.division(10,20)
 resultado = division(10,20)
 print(resultado)
-"""
 print(calculadora.__name__)
 print(__name__)
